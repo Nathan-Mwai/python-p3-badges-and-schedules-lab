f79100b6632ad5abe7a6dbb090b303d6a074b118
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-30 07:52:43 +0300

    better grasp of for loop

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,20 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badge_message = []
+    for name in names:
+        message = f"Hello, my name is {name}."
+        badge_message.append(message)
+    return badge_message
 
 def assign_rooms(names):
-    return None
+    assigned =[f"Hello, {name}! You'll be assigned to room {index + 1}!" for index, name in enumerate(names)]
+    return assigned
+
 
 def printer(names):
-    return None
+    [print(badge) for badge in batch_badge_creator(names)]
+    [print(room) for room in assign_rooms(names)]
+    # return None
+printer("John")
